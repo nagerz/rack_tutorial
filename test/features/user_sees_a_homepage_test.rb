9c3c474e7213dfbf161ae1dcc02547cc40cbd0f2
nagerz <znager@gmail.com>
2019-01-14 12:53:11 -0700

    Deploying

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,8 +4,15 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_gets_error_if_page_does_not_exist
+    visit '/error'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+
 end
